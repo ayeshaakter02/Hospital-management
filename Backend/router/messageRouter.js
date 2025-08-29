c458768b7a9b2d4cf2669cdbe7058f750cef3256
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-08-29 21:53:45 +0600

    models

--- Backend/router/messageRouter.js
+++ Backend/router/messageRouter.js
@@ -1,10 +1,13 @@
-import express from "express"
-import { getAllMessages, sendMessage } from "../controller/messageController.js";
+import express from "express";
+import {
+  getAllMessages,
+  sendMessage,
+} from "../controller/messageController.js";
 import { isAdminAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.post("/send", sendMessage)
-router.get("/getall", isAdminAuthenticated, getAllMessages)
+router.post("/send", sendMessage);
+router.get("/getall", isAdminAuthenticated, getAllMessages);
 
-export default router
+export default router;
