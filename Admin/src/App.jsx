9e25557a4aabed562988f75a7269455cee623c2c
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-08-30 22:15:43 +0600

    admin login

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useContext, useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Sidebar from "./components/Sidebar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Context } from "./main";
+import axios from "axios";
+import Loading from "./components/loading";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const { isAuthenticated, setIsAuthenticated, setUser } = useContext(Context);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/user/admin/me",
+          { withCredentials: true }
+        );
+        setIsAuthenticated(true);
+        setUser(response.data.user);
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser({});
+      }finally {
+        setLoading(false);
+      }
+    };
+    fetchUser();
+  }, [isAuthenticated,setIsAuthenticated, setUser]);
+
+
+  if (loading) {
+    return <Loading />
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <ToastContainer position="top-center" />
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
