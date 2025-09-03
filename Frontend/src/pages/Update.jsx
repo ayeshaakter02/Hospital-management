921206121cd142756a665580b8be82122c65fcfe
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-09-03 21:23:13 +0600

    mylist

--- Frontend/src/pages/Update.jsx
+++ Frontend/src/pages/Update.jsx
@@ -1,17 +1,19 @@
-// import React from 'react'
-// import Hero from '../components/Hero'
-// import MyList from '../components/MyList'
+import React from "react";
+import Hero from "../components/Hero";
+import MyList from "../components/MyList";
 
-// const Update = () => {
-//   return (
-//     <>
-//           <Hero
-//         title={"Get the Best Care Possible: Book an Appointment at Life Care Hospital"}
-//         imageUrl={"/signin.png"}
-//       />
-//       <MyList/>
-//     </>
-//   )
-// }
+const Update = () => {
+  return (
+    <>
+      <Hero
+        title={
+          "Get the Best Care Possible: Book an Appointment at Life Care Hospital"
+        }
+        imageUrl={"/signin.png"}
+      />
+      <MyList />
+    </>
+  );
+};
 
-// export default Update
+export default Update;
