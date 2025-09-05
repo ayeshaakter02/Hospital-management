9a3328a6b4a1638153fcaab60a15c4c8b2f9ba17
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-09-05 13:32:30 +0600

    update Message page

--- Frontend/src/components/MessageForm.jsx
+++ Frontend/src/components/MessageForm.jsx
@@ -88,91 +88,91 @@
 
 
 
-// import React, { useState } from "react";
-// import { toast } from "react-toastify";
-// import { ref, push } from "firebase/database";
-// import { db } from "../firebase.config";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import { ref, push } from "firebase/database";
+import { db } from "../firebase.config";
 
-// const MessageForm = () => {
-//   const [firstName, setFirstName] = useState("");
-//   const [lastName, setLastName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [phone, setPhone] = useState("");
-//   const [message, setMesage] = useState("");
+const MessageForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMesage] = useState("");
 
-//   const handleMessage = async (e) => {
-//     e.preventDefault();
+  const handleMessage = async (e) => {
+    e.preventDefault();
 
-//     try {
-//       const messagesRef = ref(db, "messages"); // database node
-//       await push(messagesRef, {
-//         firstName,
-//         lastName,
-//         email,
-//         phone,
-//         message,
-//         createdAt: new Date().toISOString(),
-//       });
+    try {
+      const messagesRef = ref(db, "messages"); // database node
+      await push(messagesRef, {
+        firstName,
+        lastName,
+        email,
+        phone,
+        message,
+        createdAt: new Date().toISOString(),
+      });
 
-//       toast.success("Message sent successfully!");
-//       setFirstName("");
-//       setLastName("");
-//       setEmail("");
-//       setPhone("");
-//       setMesage("");
-//     } catch (error) {
-//       toast.error("Failed to send message");
-//       console.error(error);
-//     }
-//   };
+      toast.success("Message sent successfully!");
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setPhone("");
+      setMesage("");
+    } catch (error) {
+      toast.error("Failed to send message");
+      console.error(error);
+    }
+  };
 
-//   return (
-//     <div className="container form-component message-form">
-//       <h2>Send Us A Message</h2>
-//       <form onSubmit={handleMessage}>
-//         <div>
-//           <input
-//             type="text"
-//             placeholder="First Name"
-//             value={firstName}
-//             onChange={(e) => setFirstName(e.target.value)}
-//           />
-//           <input
-//             type="text"
-//             placeholder="Last Name"
-//             value={lastName}
-//             onChange={(e) => setLastName(e.target.value)}
-//           />
-//         </div>
+  return (
+    <div className="container form-component message-form">
+      <h2>Send Us A Message</h2>
+      <form onSubmit={handleMessage}>
+        <div>
+          <input
+            type="text"
+            placeholder="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
 
-//         <div>
-//           <input
-//             type="text"
-//             placeholder="Email"
-//             value={email}
-//             onChange={(e) => setEmail(e.target.value)}
-//           />
-//           <input
-//             type="number"
-//             placeholder="Phone Number"
-//             value={phone}
-//             onChange={(e) => setPhone(e.target.value)}
-//           />
-//         </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="number"
+            placeholder="Phone Number"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </div>
 
-//         <textarea
-//           rows={7}
-//           placeholder="Message"
-//           value={message}
-//           onChange={(e) => setMesage(e.target.value)}
-//         ></textarea>
+        <textarea
+          rows={7}
+          placeholder="Message"
+          value={message}
+          onChange={(e) => setMesage(e.target.value)}
+        ></textarea>
 
-//         <div style={{ justifyContent: "center", alignItems: "center" }}>
-//           <button type="submit">Send</button>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
+        <div style={{ justifyContent: "center", alignItems: "center" }}>
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-// export default MessageForm;
+export default MessageForm;
