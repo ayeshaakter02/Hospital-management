931934a34ddef6143d3779ed63fe1dc5d0f4098e
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-09-05 13:03:13 +0600

    update login page

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -92,89 +92,89 @@
 
 
 
-// import React, { useContext, useState } from "react";
-// import { Context } from "../main";
-// import { Link, Navigate, useNavigate } from "react-router-dom";
-// import { toast } from "react-toastify";
-// import { auth } from "../firebase.config";
-// import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useContext, useState } from "react";
+import { Context } from "../main";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { auth } from "../firebase.config";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
-// const Login = () => {
-//   const { isAuthenticated, setIsAuthenticated } = useContext(Context);
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const navigateTo = useNavigate();
+const Login = () => {
+  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigateTo = useNavigate();
 
-//   const handleLogin = async (e) => {
-//     e.preventDefault();
-//     try {
-//       // Firebase auth দিয়ে login
-//       const userCredential = await signInWithEmailAndPassword(
-//         auth,
-//         email,
-//         password
-//       );
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      // Firebase auth দিয়ে login
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
 
-//       toast.success("Login Successful!");
-//       setIsAuthenticated(true);
-//       navigateTo("/");
-//     } catch (error) {
-//       toast.error(error.message);
-//       console.error(error);
-//     }
-//   };
+      toast.success("Login Successful!");
+      setIsAuthenticated(true);
+      navigateTo("/");
+    } catch (error) {
+      toast.error(error.message);
+      console.error(error);
+    }
+  };
 
-//   if (isAuthenticated) {
-//     return <Navigate to={"/"} />;
-//   }
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
 
-//   return (
-//     <div className="container form-component login-form">
-//       <h2>Login</h2>
-//       <p>Please Login to Continue</p>
-//       <p>
-//         Welcome to Life Care. Please manage appointments and stay
-//         connected with your healthcare provider. Your privacy and security are
-//         our top priorities. If you experience any issues, please contact our
-//         support team for assistance.
-//       </p>
-//       <form onSubmit={handleLogin}>
-//         <input
-//           type="email"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//           placeholder="Email"
-//           required
-//         />
-//         <input
-//           type="password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//           placeholder="Password"
-//           required
-//         />
+  return (
+    <div className="container form-component login-form">
+      <h2>Login</h2>
+      <p>Please Login to Continue</p>
+      <p>
+        Welcome to Life Care. Please manage appointments and stay
+        connected with your healthcare provider. Your privacy and security are
+        our top priorities. If you experience any issues, please contact our
+        support team for assistance.
+      </p>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
 
-//         <div
-//           style={{
-//             gap: "10px",
-//             justifyContent: "flex-end",
-//             flexDirection: "row",
-//           }}
-//         >
-//           <p style={{ marginBottom: 0 }}>Not Registered?</p>
-//           <Link
-//             to={"/register"}
-//             style={{ textDecoration: "none", alignItems: "center" }}
-//           >
-//             Register Now
-//           </Link>
-//         </div>
-//         <div style={{ justifyContent: "center", alignItems: "center" }}>
-//           <button type="submit">Login</button>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
+        <div
+          style={{
+            gap: "10px",
+            justifyContent: "flex-end",
+            flexDirection: "row",
+          }}
+        >
+          <p style={{ marginBottom: 0 }}>Not Registered?</p>
+          <Link
+            to={"/register"}
+            style={{ textDecoration: "none", alignItems: "center" }}
+          >
+            Register Now
+          </Link>
+        </div>
+        <div style={{ justifyContent: "center", alignItems: "center" }}>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-// export default Login;
+export default Login;
