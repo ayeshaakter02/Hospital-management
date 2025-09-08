83e10c13818cbacc54d16e37f0fae9ae4d7bd972
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-09-08 21:32:12 +0600

    fix css

--- Frontend/src/components/DoctorProfile.jsx
+++ Frontend/src/components/DoctorProfile.jsx
@@ -9,7 +9,7 @@ const DoctorProfile = () => {
           <div>
             <h6>Dr. Smita</h6>
             <p>
-              Chief Consultant & Asst. Professor MBBS, MCPS, Dip in Pediatrics, in Ad-din Hospital
+              Chief Consultant & Asst. Professor MBBS, MCPS, Dip in Pediatrics, <b>in Ad-din Hospital</b>
             </p>
           </div>
         </div>
@@ -17,7 +17,7 @@ const DoctorProfile = () => {
           <img src="/departments/doctor-female.jpg" alt="" />
           <div>
             <h6>Dr. Tamanna</h6>
-            <p>Medical Officer MBBS, Dip in Orthopedics</p>
+            <p>Medical Officer MBBS, Dip in Orthopedics, <b>in Lab-Aid Hospital</b></p>
           </div>
         </div>
         <div className="profile">
@@ -25,7 +25,7 @@ const DoctorProfile = () => {
           <div>
             <h6>Dr. Aman</h6>
             <p>
-              Assistant Professor MBBS, MCPS, MD (Pediatrics Gastroenterology)
+              Assistant Professor MBBS, MCPS, MD (Pediatrics Gastroenterology), <b>in Ad-din Hospital</b>
             </p>
           </div>
         </div>
@@ -35,21 +35,21 @@ const DoctorProfile = () => {
           <img src="/departments/doctor-male.png" alt="" />
           <div>
             <h6>Dr. Nazmul</h6>
-            <p>Assistant Professor MBBS, Dip in Orthopedics</p>
+            <p>Assistant Professor MBBS, Dip in Orthopedics, <b>in PG</b></p>
           </div>
         </div>
         <div className="profile">
           <img src="/departments/doctor-female.jpg" alt="" />
           <div>
             <h6>Dr. Ayesha</h6>
-            <p>Associate Professor MBBS, Dip in Cardiology</p>
+            <p>Associate Professor MBBS, Dip in Cardiology, <b>in Lab-Aid Hostpital</b></p>
           </div>
         </div>
         <div className="profile">
           <img src="/departments/doctor-male.png" alt="" />
           <div>
             <h6>Dr. Jakaria</h6>
-            <p>Consultant MBBS, Dip in Cardiology</p>
+            <p>Consultant MBBS, Dip in Cardiology, <b>in PG</b></p>
           </div>
         </div>
       </div>
@@ -58,21 +58,21 @@ const DoctorProfile = () => {
           <img src="/departments/doctor-female.jpg" alt="" />
           <div>
             <h6>Dr. Hafsa</h6>
-            <p>Assistant Professor MBBS, Dip in Neurology</p>
+            <p>Assistant Professor MBBS, Dip in Neurology, <b>in Neurology Hospital</b></p>
           </div>
         </div>
         <div className="profile">
           <img src="/departments/doctor-male.png" alt="" />
           <div>
             <h6>Dr. Rayaan</h6>
-            <p>Associate Professor MBBS, Dip in Neurology</p>
+            <p>Associate Professor MBBS, Dip in Neurology, <b>in Neurology Hospital</b></p>
           </div>
         </div>
         <div className="profile">
           <img src="/departments/doctor-female.jpg" alt="" />
           <div>
             <h6>Dr. tuni</h6>
-            <p>Consultant MBBS, Dip in Oncology</p>
+            <p>Consultant MBBS, Dip in Oncology, <b>in Dhaka Medical</b></p>
           </div>
         </div>
       </div>
@@ -81,21 +81,21 @@ const DoctorProfile = () => {
           <img src="/departments/doctor-male.png" alt="" />
           <div>
             <h6>Dr. Rifat</h6>
-            <p>Assistant Professor MBBS , dip in Oncology</p>
+            <p>Assistant Professor MBBS , dip in Oncology, <b>in Sir Solimullah Medical</b></p>
           </div>
         </div>
         <div className="profile">
           <img src="/departments/doctor-female.jpg" alt="" />
           <div>
             <h6>Dr. Rina</h6>
-            <p>Associate Professor MBBS, Dip in Radiology</p>
+            <p>Associate Professor MBBS, Dip in Radiology, <b>in Lab-Aid Hospital</b></p>
           </div>
         </div>
         <div className="profile">
           <img src="/departments/doctor-male.png" alt="" />
           <div>
             <h6>Dr. Parveez</h6>
-            <p>Consultant MBBS, D-Card, Dip in Radiology</p>
+            <p>Consultant MBBS, D-Card, Dip in Radiology, <b>in Islamia Hospital</b></p>
           </div>
         </div>
       </div>
@@ -104,21 +104,23 @@ const DoctorProfile = () => {
           <img src="/departments/doctor-female.jpg" alt="" />
           <div>
             <h6>Dr. Sheuly</h6>
-            <p>Medical Officer MBBS , dip in Physical Therapy</p>
+            <p>Medical Officer MBBS , dip in Physical Therapy, <b>in Neurology Hospital
+
+</b></p>
           </div>
         </div>
         <div className="profile">
           <img src="/departments/doctor-male.png" alt="" />
           <div>
             <h6>Dr. Mamun</h6>
-            <p>Associate Professor MBBS, Dip in Physical Therapy</p>
+            <p>Associate Professor MBBS, Dip in Physical Therapy, <b>in PG</b></p>
           </div>
         </div>
         <div className="profile">
           <img src="/departments/doctor-female.jpg" alt="" />
           <div>
             <h6>Dr. Ayesha</h6>
-            <p>Consultant MBBS, D-Card, Dip in Dermatology</p>
+            <p>Consultant MBBS, D-Card, Dip in Dermatology, <b>in Sir Solimullah Medical</b></p>
           </div>
         </div>
       </div>
@@ -127,21 +129,23 @@ const DoctorProfile = () => {
           <img src="/departments/doctor-male.png" alt="" />
           <div>
             <h6>Dr. Wasim</h6>
-            <p>Medical Officer MBBS , dip in Dermatology</p>
+            <p>Medical Officer MBBS , dip in Dermatology, <b>in Dhaka Medical</b></p>
           </div>
         </div>
         <div className="profile">
           <img src="/departments/doctor-female.jpg" alt="" />
           <div>
             <h6>Dr. Reshma</h6>
-            <p>Associate Professor MBBS, Dip in ENT</p>
+            <p>Associate Professor MBBS, Dip in ENT, <b>in Lad-Aid Hospital</b></p>
           </div>
         </div>
         <div className="profile">
           <img src="/departments/doctor-male.png" alt="" />
           <div>
             <h6>Dr. Abir</h6>
-            <p>Consultant MBBS, D-Card, Dip in ENT</p>
+            <p>Consultant MBBS, D-Card, Dip in ENT, <b>in Neurology Hospital
+
+</b></p>
           </div>
         </div>
       </div>
